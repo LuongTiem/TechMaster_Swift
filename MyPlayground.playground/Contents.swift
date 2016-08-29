7124e920fc5223c9bf1014086e5ced4cba01258a
Luong Tiem <luongvantiem@gmail.com>
2016-08-29 12:35:04 +0700

    First Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//var str = "Hello, playground"
+//
+//
+////-- add Objective : 1 cách thêm dữ liệu vào mảng thông qua 1 func để truyền dữ liệu động vào
+//func repeatItem<Item> (item : Item, numberOfTimes : Int )-> [Item]{
+//    
+//    
+//    var result = [Item]()
+//    
+//    for _ in 0..<numberOfTimes{
+//        result.append(item)
+//    }
+//    
+//    return result
+//    
+//}
+//
+//var test = repeatItem("BlockNow", numberOfTimes: 10)
+//print(test)
+//
+
+
+let y: Float = Float(arc4random_uniform(240)) - 80
